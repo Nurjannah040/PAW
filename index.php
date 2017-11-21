d743281d0ae4ea6ece78ff87352d02466e424942
Nurjannah040 <nurjannah040@gmail.com>
2017-11-20 20:00:17 -0800

    versi 2

--- index.php
+++ index.php
@@ -1,3 +1,99 @@
-jannah
-belajar git
-<h1> apa </h1>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title> tes 1 </title>
+		<style type= "text/css">
+		.wrapper{
+			border: 1px solid black;
+			width: 900px;
+			height: 750px;
+			margin: 20px auto 20px auto;
+			}
+				
+		.header{
+			width: 900px;
+			height: 100px;
+			margin: 20px auto 20px auto;
+			border: 1px solid black;
+		}
+		.navbar{
+			width: 900px;
+			height: 30px;
+			margin-top= 2px;
+			margin: 20px auto 20px auto;
+			border: 1px solid black;
+		}
+		
+		.conten {
+			width: 570px;
+			height: 500px;
+			margin: 20px auto 20px auto;
+			border : 1px solid black;
+			float: left;
+			margin-top: 2px;
+			margin-right: 1px;
+		}
+		
+		.sidebar {
+			width: 300px;
+			height: 500px;
+			margin: 20px auto 20px auto;
+			border : 1px solid black;
+			float: right;
+			margin-top: 2px;
+		}
+		
+		.footer {
+			width: 900px;
+			height: 60px;
+			margin-top : 550px;
+			border: 1px solid black;
+			
+		}
+		
+		</style>
+	
+	
+	
+	</head>
+	<body>
+		<!---Wrapper--->
+		<div class= "wrapper">
+		
+		<!--header-->
+		<div class="header">
+		<center>HEADER </center>
+		</div>
+		
+		<!---navbar--->
+		<div class= "navbar">
+		<center>NAVBAR </center>
+		</div>
+		<!--end navbar--->
+		
+		<!--conten-->
+		<div class="conten">
+		<center>CONTEN </center>
+		</div>
+		<!--end conten-->
+		
+		<!--sidebar--->
+		<div class="sidebar">
+		<center>SIDEBAR </center>
+		</div>
+		<!--end sidebar--->
+		
+		
+		
+		<!--footer--->
+		<div class="footer">
+		<center>FOOTER </center>
+		</div>
+		<!---end footer--->
+	
+		<!--end Wrapper--->
+		</div>
+	</body>
+</html>
+		
+	
